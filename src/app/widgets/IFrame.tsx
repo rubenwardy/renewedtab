6de58c4503f908c98884d40e87efa03a66cc9135
rubenwardy <rw@rubenwardy.com>
2021-03-03 12:42:25 +0000

    Add IFrame widget

---
+++ src/app/widgets/IFrame.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface IFrameProps {
+	url: string;
+}
+
+export default function IFrame(props: IFrameProps) {
+	return (
+		<div className="panel flush">
+			<iframe src={props.url} width="100%" height="100%" frameBorder="0" />
+		</div>);
+}
+
+
+IFrame.defaultProps = {
+	url: "https://monitor.rubenwardy.com/d-solo/46olEsqWz/overview?orgId=1&from=1614752790104&to=1614774390104&panelId=123126"
+};
--- src/app/widgets/index.tsx
+++ src/app/widgets/index.tsx
@@ -1,18 +1,20 @@
 export { default as Age } from "./Age";
 export { default as Clock } from "./Clock";
 export { default as Links } from "./Links";
+export { default as IFrame } from "./IFrame";
 export { default as Notes } from "./Notes";
 export { default as RSS } from "./RSS";
 export { default as Search } from "./Search";
 export { default as Weather } from "./Weather";
 
 import { WidgetFactory } from "../WidgetManager";
-import { Age, Clock, Links, Notes, RSS, Search, Weather } from ".";
+import { Age, Clock, Links, IFrame, Notes, RSS, Search, Weather } from ".";
 
 export const WidgetTypes: { [name: string]: WidgetFactory<any> } = {
 	Age: Age,
 	Clock: Clock,
 	Links: Links,
+	IFrame: IFrame,
 	Notes: Notes,
 	RSS: RSS,
 	Search: Search,
