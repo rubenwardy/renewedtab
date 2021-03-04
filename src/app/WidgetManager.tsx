1869a85285e4ace75eccf8fa8e90c671e6ed82db
rubenwardy <rw@rubenwardy.com>
2021-03-04 17:14:11 +0000

    Add WidgetContainer component, improve callback use

--- src/app/WidgetManager.tsx
+++ src/app/WidgetManager.tsx
@@ -1,8 +1,5 @@
 import { fromTypedJSON, toTypedJSON } from "./utils/TypedJSON";
-import { Widget } from "./components/Widget";
 import { WidgetTypes } from "./widgets";
-import React, { useState } from "react";
-
 
 type ReactFactory<T> = ((props: T) => JSX.Element) ;
 export interface WidgetFactory<T> extends ReactFactory<T> {
@@ -23,47 +20,27 @@ export interface WidgetProps<T> extends WidgetRaw<T> {
 
 export class WidgetManager {
 	private id_counter = 0;
-	private widget_props: (WidgetRaw<any>)[] = [];
-	private change_handler: (() => void) | null = null;
 
-	get widgets(): (JSX.Element)[] {
-		return this.widget_props.map(raw => {
-			const props = {
-				key: raw.id,
-				id: raw.id,
-				type: raw.type,
-				props: raw.props,
-				child: WidgetTypes[raw.type],
-				save: this.save.bind(this),
-				remove: () => this.removeWidget(raw.id),
-			};
-
-			return (<Widget {...props} />);
-		});
-	}
+	widgets: (WidgetRaw<any>)[] = [];
 
 	constructor() {
 		const json = localStorage.getItem("widgets");
 		if (json) {
-			this.widget_props = fromTypedJSON(JSON.parse(json));
+			this.widgets = fromTypedJSON(JSON.parse(json));
 			this.id_counter =
-				this.widget_props.reduce((max, widget) => Math.max(widget.id, max), 0);
+				this.widgets.reduce((max, widget) => Math.max(widget.id, max), 0);
 		} else {
 			this.resetToDefault();
 		}
 	}
 
-	onChange(handler: () => void) {
-		this.change_handler = handler;
-	}
-
 	save() {
 		console.log("Saving");
-		localStorage.setItem("widgets", JSON.stringify(toTypedJSON(this.widget_props)));
+		localStorage.setItem("widgets", JSON.stringify(toTypedJSON(this.widgets)));
 	}
 
 	resetToDefault() {
-		this.widget_props = [];
+		this.widgets = [];
 		["Links", "Notes", "Age", "Weather", "RSS"].forEach(this.createWidget.bind(this));
 	}
 
@@ -72,7 +49,7 @@ export class WidgetManager {
 
 		const widget = WidgetTypes[type];
 
-		this.widget_props.push({
+		this.widgets.push({
 			id: this.id_counter,
 			type: type,
 			props: Object.assign({}, widget.defaultProps)
@@ -82,25 +59,13 @@ export class WidgetManager {
 
 	removeWidget(id: number) {
 		console.log("Remove widget by id ", id);
-		var i = this.widget_props.length;
+		var i = this.widgets.length;
 		while (i--) {
-			if (this.widget_props[i].id == id) {
-				console.log("- found and deleted ", this.widget_props[i]);
-				this.widget_props.splice(i, 1);
+			if (this.widgets[i].id == id) {
+				console.log("- found and deleted ", this.widgets[i]);
+				this.widgets.splice(i, 1);
 			}
 		}
 		this.save();
-
-		if (this.change_handler) {
-			this.change_handler();
-		}
 	}
-}
-
-
-const _wm = new WidgetManager();
-export function useWidgetManager() {
-	const [_, updateState] = useState<any>(null);
-	_wm.onChange(() => updateState({}));
-	return _wm;
 }
--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -1,26 +1,44 @@
 import React, { useState } from "react";
 import { Widget } from "./Widget";
-import { useWidgetManager } from "app/WidgetManager";
-import { Clock, Search } from "app/widgets";
+import { WidgetManager } from "app/WidgetManager";
+import { Clock, Search, WidgetTypes } from "app/widgets";
 import CreateWidgetDialog from "./CreateWidgetDialog";
 
-function App(_props: any) {
-	const widgetManager = useWidgetManager();
-	const [createVisible, setCreateVisible] = useState(false);
+const widgetManager = new WidgetManager();
 
-	const widgets = widgetManager.widgets;
+function WidgetContainer(_props: any) {
+	const [_, setUpdate] = useState({});
+
+	function handleRemove(id: number) {
+		widgetManager.removeWidget(id);
+		setUpdate({});
+	}
+
+	const widgets = widgetManager.widgets.map(raw => (
+		<Widget key={raw.id} id={raw.id}
+			type={raw.type} props={raw.props}
+			child={WidgetTypes[raw.type]}
+			save={widgetManager.save.bind(widgetManager)}
+			remove={() => handleRemove(raw.id)} />));
+
+	return (
+		<main className={widgets.length > 5 ? "main-wide" : ""}>
+			<Clock showSeconds={false} />
+			<Widget type="Search" id={0} save={() => {}} remove={() => {}}
+				child={Search} props={{searchTitle: "DuckDuckGo", searchURL: "https://duckduckgo.com"}} />
+			<div className="grid">
+				{widgets}
+			</div>
+		</main>);
+}
+
+function App(_props: any) {
+	const [createVisible, setCreateVisible] = useState(false);
 	return (
 		<div>
 			<CreateWidgetDialog visible={createVisible} manager={widgetManager} onClose={() => setCreateVisible(false)} />
-			<main className={widgets.length > 5 ? "main-wide" : ""}>
-				<Clock showSeconds={false} />
-				<Widget type="Search" id={0} save={() => {}} remove={() => {}}
-					child={Search} props={{searchTitle: "DuckDuckGo", searchURL: "https://duckduckgo.com"}} />
-				<div className="grid">
-					{widgets}
-					<a className="btn" onClick={() => setCreateVisible(true)}>+</a>
-				</div>
-			</main>
+			<WidgetContainer />
+			<a className="btn" onClick={() => setCreateVisible(true)}>Add Widget</a>
 		</div>);
 }
 
