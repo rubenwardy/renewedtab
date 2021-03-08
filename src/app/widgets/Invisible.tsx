363773e8d70732ba417375bfd742f16194214029
rubenwardy <rw@rubenwardy.com>
2021-03-08 19:37:23 +0000

    Add Invisible widget

---
+++ src/app/widgets/Invisible.tsx
@@ -0,0 +1,12 @@
+import { Vector2 } from 'app/utils/Vector2';
+import React from 'react';
+
+export default function Invisible(_props: any) {
+	return (<></>);
+}
+
+Invisible.defaultProps = {};
+
+Invisible.schema = {};
+
+Invisible.defaultSize = new Vector2(5, 5);
