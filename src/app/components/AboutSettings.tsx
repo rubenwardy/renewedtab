3351515afa01b72787cf167a6a1ac144cee1e015
rubenwardy <rw@rubenwardy.com>
2021-03-08 23:24:38 +0000

    Add ability to reset everything from settings modal

--- src/app/components/AboutSettings.tsx
+++ src/app/components/AboutSettings.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
 export default function AboutSettings(_props: any) {
+	function handleReset() {
+		localStorage.clear();
+		location.reload();
+	}
+
 	return (
 		<div className="modal-body">
 			<p>
@@ -11,6 +16,9 @@ export default function AboutSettings(_props: any) {
 				Created by <a href="https://rubenwardy.com">rubenwardy</a>.
 				Licensed under GPLv2 or later,&nbsp;
 				<a href="https://gitlab.com/rubenwardy/homescreen/">source code</a>.
+			</p>
+			<p>
+				<a className="btn btn-danger" onClick={handleReset}>Reset everything</a>
 			</p>
 		</div>);
 }
