d3fedbf060496d8809e21bc97aa0edff017a8fd5
rubenwardy <rw@rubenwardy.com>
2021-03-17 14:50:18 +0000

    Fix CORS header methods on API server

--- src/server/server.ts
+++ src/server/server.ts
@@ -32,10 +32,10 @@ const app = express();
 app.use(express.json());
 
 app.use((_req, res, next) => {
-	const expiresAt = new Date(new Date().getTime() + 4*60*60*1000);
+	const expiresAt = new Date(new Date().getTime() + 15*60*1000);
 
 	res.append("Access-Control-Allow-Origin", ["*"]);
-	res.append("Access-Control-Allow-Methods", "GET");
+	res.append("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
 	res.append("Access-Control-Allow-Headers", "Content-Type");
 	res.append("expires", expiresAt.toISOString());
 	next();
