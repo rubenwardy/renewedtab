1340fa0a3e2aca689a1c8155273cf95d975a882b
rubenwardy <rw@rubenwardy.com>
2021-04-10 15:27:43 +0100

    Fix deepCopy breaking dates and vectors

--- src/app/utils/deepcopy.tsx
+++ src/app/utils/deepcopy.tsx
@@ -1,9 +1,19 @@
+import { Vector2 } from "./Vector2";
+
 export default function deepCopy(value: any) {
 	if (typeof value !== "object" || value === null) {
 		return value;
 	}
 
+	if (value instanceof Date) {
+		return new Date(value);
+	} else if (value instanceof Vector2) {
+		return new Vector2(value.x, value.y);
+	}
+
 	let retval: any = Array.isArray(value) ? [] : {};
+	Object.setPrototypeOf(retval, Object.getPrototypeOf(value));
+
 	for (let key in value) {
 		retval[key] = deepCopy(value[key]);
 	}
