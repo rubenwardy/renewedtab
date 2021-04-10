b5d46c7adda962f63eb37ce95443bc42ed3b7270
rubenwardy <rw@rubenwardy.com>
2021-04-10 10:25:02 +0100

    Clean up widget index

--- src/app/widgets/index.tsx
+++ src/app/widgets/index.tsx
@@ -1,24 +1,22 @@
-export { default as Age } from "./Age";
-export { default as Button } from "./Button";
-export { default as Clock } from "./Clock";
-export { default as DailyGoal } from "./DailyGoal";
-export { default as Greeting } from "./Greeting";
-export { default as HelpAbout } from "./HelpAbout";
-export { default as IFrame } from "./IFrame";
-export { default as Image } from "./Image";
-export { default as Invisible } from "./Invisible";
-export { default as Links } from "./Links";
-export { default as Notes } from "./Notes";
-export { default as Feed } from "./Feed";
-export { default as Search } from "./Search";
-export { default as SpaceFlights } from "./SpaceFlights";
-export { default as TopSites } from "./TopSites";
-export { default as WebComic } from "./WebComic";
-export { default as Weather } from "./Weather";
-
 import { WidgetType } from "../Widget";
-import { Age, Button, Clock, DailyGoal, Greeting, HelpAbout, IFrame, Image, Invisible,
-	Links, Notes, Feed, Search, SpaceFlights, TopSites, WebComic, Weather } from ".";
+
+import Age from "./Age";
+import Button from "./Button";
+import Clock from "./Clock";
+import DailyGoal from "./DailyGoal";
+import Feed from "./Feed";
+import Greeting from "./Greeting";
+import HelpAbout from "./HelpAbout";
+import IFrame from "./IFrame";
+import Image from "./Image";
+import Invisible from "./Invisible";
+import Links from "./Links";
+import Notes from "./Notes";
+import Search from "./Search";
+import SpaceFlights from "./SpaceFlights";
+import TopSites from "./TopSites";
+import Weather from "./Weather";
+import WebComic from "./WebComic";
 
 export const WidgetTypes: { [name: string]: WidgetType<any> } = {
 	Age: Age,
