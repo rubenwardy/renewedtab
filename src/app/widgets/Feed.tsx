361679daa64b4175ef8e46a53a545f1202a1ec28
rubenwardy <rw@rubenwardy.com>
2021-04-20 19:46:12 +0100

    Add filter to Feed widget

--- src/app/widgets/Feed.tsx
+++ src/app/widgets/Feed.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Vector2 } from 'app/utils/Vector2';
 import Schema, { type } from 'app/utils/Schema';
-import { useFeed } from 'app/hooks/feeds';
-import { WidgetProps } from 'app/Widget';
+import { Feed, useFeed } from 'app/hooks/feeds';
+import { Widget, WidgetProps } from 'app/Widget';
 import { defineMessages, useIntl } from 'react-intl';
 import schemaMessages from 'app/locale/common';
 
@@ -19,11 +19,30 @@ const messages = defineMessages({
 	loading: {
 		defaultMessage: "Loading feed...",
 	},
+
+	filters: {
+		defaultMessage: "Filter Articles",
+	},
+
+	filtersHint: {
+		defaultMessage: "An article will be shown if it matches at least one 'Allow'ed filter, and none of the other filters.",
+	},
+
+	isAllowed: {
+		defaultMessage: "Allow",
+	},
 });
+
+interface Filter {
+	id: string; //< required for React
+	isAllowed: boolean;
+	text: string;
+}
 
 interface FeedProps {
 	title?: string;
 	url: string;
+	filters: Filter[];
 }
 
 export default function Feed(widget: WidgetProps<FeedProps>) {
@@ -39,8 +58,22 @@ export default function Feed(widget: WidgetProps<FeedProps>) {
 			</div>);
 	}
 
-	const rows = feed.articles.map(article =>
-		(<li key={article.link}><a href={article.link}>{article.title}</a></li>));
+	const allowFilters = props.filters
+		.filter(filter => filter.isAllowed)
+		.map(filter => filter.text.toLowerCase());
+
+	const blockFilters = props.filters
+		.filter(filter => !filter.isAllowed)
+		.map(filter => filter.text.toLowerCase());
+
+	const rows = feed.articles
+		.filter(article => {
+			const title = article.title.toLowerCase();
+			return allowFilters.every(filter => title.includes(filter)) &&
+					!blockFilters.some(filter => title.includes(filter));
+		})
+		.map(article => (
+			<li key={article.link}><a href={article.link}>{article.title}</a></li>));
 
 	const title = (props.title && props.title.length > 0)
 		? props.title
@@ -65,9 +98,20 @@ Feed.initialProps = {
 	url: "https://feeds.bbci.co.uk/news/rss.xml"
 };
 
+
+const filterSchema: Schema = {
+	isAllowed: type.boolean(messages.isAllowed),
+	text: type.string(schemaMessages.text),
+};
+
 Feed.schema = {
 	title: type.string(schemaMessages.title, messages.titleHint),
 	url: type.urlPerm(schemaMessages.url, schemaMessages.rssUrlHint),
+	filters: type.array(filterSchema, messages.filters, messages.filtersHint),
 } as Schema;
 
 Feed.defaultSize = new Vector2(5, 4);
+
+Feed.onLoaded = async (widget: Widget<FeedProps>) => {
+	widget.props.filters = widget.props.filters ?? [];
+}
