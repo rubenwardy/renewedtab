a95a6f1b37d495d19a4f2a0172655b9ef14f377e
rubenwardy <rw@rubenwardy.com>
2021-05-01 19:48:11 +0100

    Release 1.3.0

--- src/server/server.ts
+++ src/server/server.ts
@@ -16,8 +16,8 @@ export const serverConfig = (function() {
 export const IS_DEBUG = process.env.NODE_ENV !== "production";
 export const OWNER_EMAIL = process.env.OWNER_EMAIL ?? serverConfig.OWNER_EMAIL;
 const DISCORD_WEBHOOK = process.env.DISCORD_WEBHOOK ?? serverConfig.DISCORD_WEBHOOK;
-export const UA_DEFAULT = "Mozilla/5.0 (compatible; Renewed Tab App/1.2.0; +https://renewedtab.rubenwardy.com/)";
-export const UA_PROXY = "Mozilla/5.0 (compatible; Renewed Tab Proxy/1.2.0; +https://renewedtab.rubenwardy.com/)";
+export const UA_DEFAULT = "Mozilla/5.0 (compatible; Renewed Tab App/1.3.0; +https://renewedtab.rubenwardy.com/)";
+export const UA_PROXY = "Mozilla/5.0 (compatible; Renewed Tab Proxy/1.3.0; +https://renewedtab.rubenwardy.com/)";
 
 
 // App
