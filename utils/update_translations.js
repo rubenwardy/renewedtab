12906ed4e5d5e9c35426964bb40c0c5322d453db
rubenwardy <rw@rubenwardy.com>
2021-05-18 00:29:03 +0100

    Add update_translations.js script

---
+++ utils/update_translations.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function readJSON(path) {
+	return JSON.parse(fs.readFileSync(path).toString());
+}
+
+function writeJSON(path, data) {
+	fs.writeFileSync(path, JSON.stringify(data, null, 2));
+}
+
+
+const english = readJSON("src/app/locale/en.json");
+const dir = fs.opendirSync('src/app/locale');
+
+let item;
+while ((item = dir.readSync()) !== null) {
+	if (item.name.endsWith(".json") && item.name != "en.json") {
+		const path = 'src/app/locale/' + item.name;
+
+		const current = readJSON(path);
+
+		for (let key in current) {
+			if (!english.hasOwnProperty(key)) {
+				current[key] = undefined;
+			}
+		}
+
+		for (let key in english) {
+			if (!current.hasOwnProperty(key)) {
+				current[key] = english[key];
+			}
+		}
+
+		writeJSON(path, current);
+	}
+}
+
+dir.closeSync();
