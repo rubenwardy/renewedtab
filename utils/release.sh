ec8faf40f7ebd057b6765f6c8a221fc35db6bb9f
rubenwardy <rw@rubenwardy.com>
2021-05-22 17:17:27 +0100

    Set homepage on Firefox
    Fixes #134

--- utils/release.sh
+++ utils/release.sh
@@ -28,6 +28,7 @@ tag=$(npm -no-git-tag-version version ${type})
 version=${tag:1}
 sed -i "s/\/$old_version/\/$version/g" src/server/index.ts
 sed -i "s/\"$old_version\"/\"$version\"/g" src/webext/manifest.json
+sed -i "s/\"$old_version\"/\"$version\"/g" src/webext/manifest.firefox.json
 
 git add .
 git commit -m "Release $version"
