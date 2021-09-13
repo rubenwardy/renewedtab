414c3869bbc6afb3b9b8f684b4f3e973398bf6ac
rubenwardy <rw@rubenwardy.com>
2021-09-13 21:11:46 +0100

    Add script to check translation variables

---
+++ utils/check_translations.js
@@ -0,0 +1,43 @@
+/**
+ * Checks that translations contain the same variables
+ */
+
+const fs = require('fs');
+
+function readJSON(path) {
+	return JSON.parse(fs.readFileSync(path).toString());
+}
+
+const english = readJSON("src/app/locale/en.json");
+
+const dir = fs.opendirSync('src/app/locale');
+
+let hasError = false;
+
+let item;
+while ((item = dir.readSync()) !== null) {
+	if (item.name.endsWith(".json") && item.name != "en.json") {
+		const path = 'src/app/locale/' + item.name;
+
+		const current = readJSON(path);
+
+		const rVariable = /(\{[a-z]+\}|\<[a-z]+\>)/g;
+		Object.entries(english).forEach(([key, value]) => {
+			const translated = current[key];
+			if (!translated) {
+				return;
+			}
+
+			[...value.message.matchAll(rVariable)].forEach(match => {
+				if (!translated.message.includes(match[0])) {
+					console.error(`${path}: translation ${key} missing ${match[0]} variable`);
+					hasError = true;
+				}
+			});
+		})
+	}
+}
+
+dir.closeSync();
+
+process.exit(hasError ? 1 : 0);
