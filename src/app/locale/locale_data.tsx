d491aba349c3fa927cdf0501db0e863801e4e538
rubenwardy <rw@rubenwardy.com>
2025-02-11 23:08:01 +0000

    Enable BE, JA, LT, SV translations

--- src/app/locale/locale_data.tsx
+++ src/app/locale/locale_data.tsx
@@ -4,14 +4,18 @@ type Translation = Record<string, MessageFormatElement[]>;
 
 const locales : { [key: string]: Translation } = {
 	"en": require("./compiled/en.json"),
+	"be": require("./compiled/be.json"),
 	"bg": require("./compiled/bg.json"),
 	"de": require("./compiled/de.json"),
 	"es": require("./compiled/es.json"),
 	"fr": require("./compiled/fr.json"),
 	"it": require("./compiled/it.json"),
+	"ja": require("./compiled/ja.json"),
+	"lt": require("./compiled/lt.json"),
 	"ms": require("./compiled/ms.json"),
 	"pt-br": require("./compiled/pt_BR.json"),
 	"ru": require("./compiled/ru.json"),
+	"sv": require("./compiled/sv.json"),
 	"tr": require("./compiled/tr.json"),
 	"zh-cn": require("./compiled/zh_CN.json"),
 	"zh-tw": require("./compiled/zh_TW.json"),
